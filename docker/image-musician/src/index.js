5d5c67ebfe492893e46ef3e4a04b0520f208a7b5
Kitsunemma <yecomte@gmail.com>
2022-06-07 20:36:30 +0200

    add app structur

---
+++ docker/image-musician/src/index.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let app = express();
+
+let logdate = () => (new Date()).toUTCString();
+
+app.get('/fortune-cookies/', (req, res) => {
+    console.log([logdate(), req.ip, req.method, req.url].join(' '));
+
+    res.send({});
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(logdate() + ' Now listening on port ' + PORT + '!');
+});
